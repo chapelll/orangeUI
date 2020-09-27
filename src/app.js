a505f63cc236884fce6714895a71c8498f9797f6
chapelll <214426857@qq.com>
2020-09-27 15:46:00 +0800

    使用chai添加测试用例

--- src/app.js
+++ src/app.js
@@ -5,6 +5,50 @@ import Icon from './icon.vue'
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 
-new Vue ({
+new Vue({
     el: '#app',
 })
+
+import chai from 'chai'
+const expect = chai.expect
+
+// 单元测试
+{
+    let Constructor = Vue.extend(Button)
+    let vm = new Constructor({
+        propsData: {
+            icon: 'setting'
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-setting')
+    vm.$destroy()
+}
+{
+    let Constructor = Vue.extend(Button)
+    let vm = new Constructor({
+        propsData: {
+            icon: 'buy'
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-buy')
+    vm.$destroy()
+}
+{
+    let Constructor = Vue.extend(Button)
+    let vm = new Constructor({
+        propsData: {
+            icon: 'good'
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-good')
+    vm.$destroy()
+}
