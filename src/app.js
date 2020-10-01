03dbd06e222c996ed4d44be01a534fac7accba8f
chapelll <214426857@qq.com>
2020-10-01 21:16:44 +0800

    row和col的雏形

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import Button from './button.vue'
 import Icon from './icon.vue'
 import Input from './input.vue'
+import Row from './row.vue'
+import Col from './col.vue'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-input', Input)
+Vue.component('g-row', Row)
+Vue.component('g-col', Col)
 
 new Vue({
     el: '#app',
