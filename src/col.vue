f5b99e4bf0fd5cd90d61bc4fdcfeeca20aa54e38
chapelll <214426857@qq.com>
2020-10-02 20:47:18 +0800

    初步完成grid组件

--- src/col.vue
+++ src/col.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="col" :class="colClass" :style="colStyle">
-      <slot></slot>
+    <slot></slot>
   </div>
 </template>
 
 <script>
+let validator = (value) => {
+  let keys = Object.keys(value);
+  let valid = true;
+  keys.forEach((key) => {
+    if (!["span", "offset"].includes(key)) {
+      valid = false;
+    }
+  });
+  return valid;
+};
+
 export default {
   name: "OrangeCol",
+  props: {
+    span: {
+      type: [Number, String],
+    },
+    offset: {
+      type: [Number, String],
+    },
+    ipad: { type: Object, validator },
+    pc: { type: Object, validator },
+  },
   data() {
     return {
       gutter: 0,
@@ -20,19 +41,19 @@ export default {
       };
     },
     colClass() {
-      let span = this.span
-      let offset = this.offset
-      return [span && `col-${span}`, offset && `offset-${offset}`]
+      let span = this.span;
+      let offset = this.offset;
+      let ipad = this.ipad;
+      let pc = this.pc;
+      return [
+        span && `col-${span}`,
+        offset && `offset-${offset}`,
+        ...(ipad ? [`col-ipad-${ipad.span}`] : []),
+        ...(pc ? [`col-pc-${pc.span}`] : []),
+      ];
     },
   },
-  props: {
-    span: {
-      type: [Number, String],
-    },
-    offset: {
-      type: [Number, String],
-    },
-  },
+  
 };
 </script>
 
@@ -42,12 +63,11 @@ export default {
   // height: 50px;
   // background: grey;
   // border: 1px solid red;
-  // .col.col-1
   $class-prefix: col-;
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
       width: ($n / 24) * 100%;
-    }
+    } 
   }
   $class-prefix: offset-;
   @for $n from 1 through 24 {
@@ -56,5 +76,37 @@ export default {
     }
   }
   //设置偏移量offset(margin-left)
+
+  @media (min-width: 577px) {
+    //设备宽度在577px之上视为ipad
+    $class-prefix: col-ipad-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+    $class-prefix: offset-ipad-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+
+  @media (min-width: 993px) {
+    //设备宽度在993px之上视为PC屏幕
+    $class-prefix: col-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+    $class-prefix: offset-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
 }
 </style>
