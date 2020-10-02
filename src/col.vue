0ce78d7a7722a83fa1f446cfa3a4c175a8c7bff1
chapelll <214426857@qq.com>
2020-10-02 15:24:08 +0800

    重构col和row

--- src/col.vue
+++ src/col.vue
@@ -1,12 +1,6 @@
 <template>
-  <div
-    class="col"
-    :class="[span && `col-${span}`, offset && `offset-${offset}`]"
-    :style="{paddingLeft: gutter/2 + 'px',paddingRight: gutter/2 + 'px'}"
-  >
-    <div style="border: 1px solid green; height: 100px">
+  <div class="col" :class="colClass" :style="colStyle">
       <slot></slot>
-    </div>
   </div>
 </template>
 
@@ -15,8 +9,21 @@ export default {
   name: "OrangeCol",
   data() {
     return {
-      gutter: 0
-    }
+      gutter: 0,
+    };
+  },
+  computed: {
+    colStyle() {
+      return {
+        paddingLeft: this.gutter / 2 + "px",
+        paddingRight: this.gutter / 2 + "px",
+      };
+    },
+    colClass() {
+      let span = this.span
+      let offset = this.offset
+      return [span && `col-${span}`, offset && `offset-${offset}`]
+    },
   },
   props: {
     span: {
--- src/row.vue
+++ src/row.vue
@@ -1,8 +1,5 @@
 <template>
-  <div
-    class="row"
-    :style="{ marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px' }"
-  >
+  <div class="row" :style="rowStyle">
     <slot></slot>
   </div>
 </template>
@@ -15,12 +12,20 @@ export default {
       type: [Number, String],
     },
   },
+  computed: {
+    rowStyle() {
+      return {
+        marginLeft: -this.gutter / 2 + "px",
+        marginRight: -this.gutter / 2 + "px",
+      };
+    },
+  },
   mounted() {
     // 当父组件挂载时，所有的子组件都已经挂载到父亲身上
     // 此时找到各个子组件，将父亲的gutter传给它们
-    this.$children.forEach((vm)=>{
-      vm.gutter = this.gutter
-    })
+    this.$children.forEach((vm) => {
+      vm.gutter = this.gutter;
+    });
   },
 };
 </script>
