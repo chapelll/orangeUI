33e5a0064a981b82605587b696601bdf1b0176ef
chapelll <214426857@qq.com>
2020-10-03 17:32:32 +0800

    基本完成layout布局样式

--- src/app.js
+++ src/app.js
@@ -4,12 +4,22 @@ import Icon from './icon.vue'
 import Input from './input.vue'
 import Row from './row.vue'
 import Col from './col.vue'
+import Layout from './layout.vue'
+import Header from './header.vue'
+import Footer from './footer.vue'
+import Sider from './sider.vue'
+import Content from './content.vue'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-input', Input)
 Vue.component('g-row', Row)
 Vue.component('g-col', Col)
+Vue.component('g-layout', Layout)
+Vue.component('g-header', Header)
+Vue.component('g-footer', Footer)
+Vue.component('g-sider', Sider)
+Vue.component('g-content', Content)
 
 new Vue({
     el: '#app',
---
+++ src/layout.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="layout" :class="layoutClass">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OrangeLayout",
+  data() {
+    return {
+      layoutClass: {
+        hasSider: false
+      }
+    }
+  },
+  mounted() {
+    //在mounted阶段查看子组件中是否有sider组件
+    //有就给这个layout加上hasSider这个类名
+      this.$children.forEach((vm)=> {
+        if (vm.$options.name === 'OrangeSider') {
+          this.layoutClass.hasSider = true
+        }
+      })
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+    .layout {
+      flex-grow: 1;
+      display: flex;
+      flex-direction: column;
+      border: 1px solid red;
+      &.hasSider {
+        flex-direction: row;
+      }
+    }
+</style>
