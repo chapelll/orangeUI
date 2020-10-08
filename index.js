a507dc49f1dfdc36f436f1ef107f12256317c9c3
chapelll <214426857@qq.com>
2020-10-08 21:27:21 +0800

    更新index.js(包入口文件)

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-import Button from './src/button.vue'
-import Icon from './src/Icon.vue'
+import Button from './src/Button'
+import Icon from './src/Icon'
+import Col from './src/Col'
+import Collapse from './src/Collapse'
+import CollapseItem from './src/Collapse-item'
+import Content from './src/Content'
+import Footer from './src/Footer'
+import Header from './src/Header'
+import Input from './src/Input'
+import Layout from './src/Layout'
+import Row from './src/Row'
+import Sider from './src/Sider'
+import Tabs from './src/Tabs'
+import TabsBody from './src/Tabs-body'
+import TabsHead from './src/Tabs-head'
+import TabsItem from './src/Tabs-item'
+import TabsPane from './src/Tabs-pane'
+import Toast from './src/Toast'
+import plugin from './src/plugin'
 
-export {Button, Icon}
+
+export {
+    Button, Icon, Col, Collapse, CollapseItem, Content, Footer, Header, Input, Layout
+    , Row, Sider, Tabs, TabsHead, TabsBody, TabsItem, TabsPane, Toast, plugin
+}
